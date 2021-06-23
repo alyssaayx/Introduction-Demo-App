418471c96f26789109e5108ecca9993fdc2dc42a
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-06-23 09:19:55 +0800

    Initial Commit

---
+++ Introduction Demo App/Introduction_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introduction_Demo_AppApp.swift
+//  Introduction Demo App
+//
+//  Created by rgs on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Introduction_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
